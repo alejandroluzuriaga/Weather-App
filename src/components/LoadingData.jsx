36ef860f1ca9126283997b4a83780f4531774996
alejandroluzuriaga <luzuriagalejandro@gmail.com>
2023-04-08 14:24:52 +0200

    Loading component styles fixed

--- src/components/LoadingData.jsx
+++ src/components/LoadingData.jsx
@@ -9,7 +9,7 @@ const LoadingData = () => {
         </div>
         <div className="img-temp-container">
           <div className="weather-img">
-            <img src="https://www.freeiconspng.com/uploads/blue-smiley-face-png-31.png" alt="Actual Weather img" />
+            <img src="https://res.cloudinary.com/djgkryxou/image/upload/v1680956209/Weather-App/face-png-42675_xazdb8.png" alt="Happy Face" />
           </div>
           <div className='temperature'>
             <h1>Loading</h1>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -9,7 +9,7 @@ const NotFound = () => {
         </div>
         <div className="img-temp-container">
           <div className="weather-img">
-            <img src="https://cdn.freebiesupply.com/logos/large/2x/facebook-sad-logo-black-and-white.png" alt="Actual Weather img" />
+            <img src="https://res.cloudinary.com/djgkryxou/image/upload/v1680956280/Weather-App/sad-logo-black-and-white_gockhk.png" alt="Sad face" />
           </div>
           <div className='temperature'>
             <h1>Ups...</h1>
