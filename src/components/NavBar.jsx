04befcc1fc083458d3e76e0c269450dfe12ce496
alejandroluzuriaga <luzuriagalejandro@gmail.com>
2023-04-08 12:36:06 +0200

    Responsive design OK

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,48 +1,62 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {NavLink} from 'react-router-dom'
 
 const NavBar = () => {
+
   return (
     <header className="header-container">
-        <nav>
-            <ul>
-                <li>
-                <NavLink style={({isActive}) =>{
-                    return isActive ? {
-                        textTransform:"uppercase", 
-                        color: "white", 
-                        textDecorationLine:"underline"} : {}
-                }}
-                to="/"
-                >
-                    <p className='navbar-element'>Weather now</p>
-                    </NavLink>
-                </li>
-                <li>
-                <NavLink style={({isActive}) =>{
-                    return isActive ? {
-                        textTransform:"uppercase", 
-                        color: "white", 
-                        textDecorationLine:"underline"} : {}
-                }}
-                to="/actual-weekly"
-                >
-                    <p className='navbar-element'>This week</p></NavLink>
-                </li>
-                <li>
-                    <NavLink style={({isActive}) =>{
-                        return isActive ? {
-                            textTransform:"uppercase", 
-                            color: "white", 
-                            textDecorationLine:"underline"} : {}
-                    }}
-                    to="/othercities-weekly"
-                    >
-                        <p className='navbar-element'>Check other cities</p>
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
+      <nav>
+        <ul className="nav-links nav-active">
+          <li>
+            <NavLink
+              style={({ isActive }) => {
+                return isActive
+                  ? {
+                      textTransform: "uppercase",
+                      color: "white",
+                      textDecorationLine: "underline",
+                    }
+                  : {};
+              }}
+              to="/"
+            >
+              <p className="navbar-element">Weather now</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              style={({ isActive }) => {
+                return isActive
+                  ? {
+                      textTransform: "uppercase",
+                      color: "white",
+                      textDecorationLine: "underline",
+                    }
+                  : {};
+              }}
+              to="/actual-weekly"
+            >
+              <p className="navbar-element">This week</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              style={({ isActive }) => {
+                return isActive
+                  ? {
+                      textTransform: "uppercase",
+                      color: "white",
+                      textDecorationLine: "underline",
+                    }
+                  : {};
+              }}
+              to="/othercities-weekly"
+            >
+              <p className="navbar-element">Check other cities</p>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
